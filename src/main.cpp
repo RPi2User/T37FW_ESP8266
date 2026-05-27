27dced88f97e7bb10cb2eb5660f0acea1ec0c7f1
RPi2User <florian@hatzfeld.biz>
2026-05-27 23:58:48 +0200

    Tortured myself with UART shenanigans

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include "tty.h"
+
+SoftwareSerial softSerial(D6, D5, 1); // RX, TX
 
 void setup() {
    pinMode(LED_BUILTIN, OUTPUT);
-   pinMode(D5, OUTPUT);
-   pinMode(D0, INPUT);
-   TTY_Fox();
-   setLoopback(1);
+   //pinMode(A0, INPUT);  // -> RX
+   //pinMode(D5, OUTPUT);
+
+   softSerial.begin(9600);
+   delay(100);
+   softSerial.write('A');
+
+   //TTY_Fox();
+   //setLoopback(1);
    // put your setup code here, to run once:
 }
 
-void loop() {
-   TTY_ReadKey();
-
-   delay(10);
+void loop() { 
+   if (softSerial.available()) {
+      digitalWrite(LED_BUILTIN, LOW);
+      int c = softSerial.read();
+      softSerial.write(c);
+   }
+   else {
+      delay(10);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+   }
 }
